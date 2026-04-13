83b36cad3272b0731159307b8526b4a00e133ef0
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-04-13 20:10:28 +0530

    Done

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@google/generative-ai', '@googlemaps/js-api-loader']
-  }
+  serverExternalPackages: ['@google/generative-ai', '@googlemaps/js-api-loader']
 };
 
 export default nextConfig;
